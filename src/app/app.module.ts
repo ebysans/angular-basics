2c1988d1e6d895d5258841fa9a8dbf95f9fb122e
ebysans <36588004+ebysans@users.noreply.github.com>
2018-08-19 21:35:12 +0800

    angular routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,26 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 import { DishService} from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +40,8 @@ import { DishService} from './services/dish.service';
     FlexLayoutModule,
     MatListModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
