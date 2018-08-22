a0afb571948414bd4eafc329c84fa4437bcba573
ebysans <36588004+ebysans@users.noreply.github.com>
2018-08-22 23:14:42 +0800

    single page application2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,12 @@
 // we import the 'Input' module which helps supply information from a component to another component
-import { Component, OnInit, Input } from '@angular/core';
-import {Dish} from '../shared/dish';
+//import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 //import { DISHES } from '../shared/dishes';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -9,14 +14,21 @@ import {Dish} from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()
+ // @Input()
   dish: Dish;
 
   //selectedDish: Dish = DISHES[0];
 
-  constructor() { }
+  constructor(private dishservice:DishService, 
+    private route: ActivatedRoute,
+    private location: Location ) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dish= this.dishservice.getDish(id);
+  }
+  goBack(): void {
+    this.location.back();
   }
 
 }
